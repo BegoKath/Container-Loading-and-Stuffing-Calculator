080612905faa369840c8ff6d3406f6cc81bedb75
Soporte1 <soporte1@iconta.ec>
2022-11-17 15:22:44 -0500

    make

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-//app.js
 const express = require('express');
-const http = require('http');
+const favicon = require('express-favicon');
 const path = require('path');
-let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/public/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+app.listen(port);

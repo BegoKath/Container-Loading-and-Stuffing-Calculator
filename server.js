ef3057d61d1d46e7b456083fe01c1566ea1b0565
Soporte1 <soporte1@iconta.ec>
2022-11-17 11:21:45 -0500

    new commit

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
+//app.js
 const express = require('express');
-const favicon = require('express-favicon');
+const http = require('http');
 const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
